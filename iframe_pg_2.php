66fc66353309ea65d32644ab6675625df6bf52a7
Joze Perez <jozeperezp@gmail.com>
2011-12-20 16:03:08 -0500

    Added iframe_pg_2.php

---
+++ iframe_pg_2.php
@@ -0,0 +1,52 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Insert Dealer Name Here</title>
+    <link type="text/css" rel="stylesheet" href="iframe.css" />
+</head>
+<body>
+	<!-- start container -->
+    <div id="container">
+    	<!-- start main -->
+		<div id="main">     
+            <!-- start content -->
+            <div class="content">
+	            <div class="title_detail"><h1>Toyota Yaris 2010</h1></div>
+            
+            	<div class="box_detail_2">
+		            <div class="thumb_med">
+			            <a href="http://midealervirtual.com/vpics/web/c6v2316t111006175246.JPG" title="Toyota Yaris 2010 en Automarca Kia"><img src="http://midealervirtual.com/vpics/med/med_c6v2316t111006175246.JPG" title="Toyota Yaris 2010 en Automarca Kia" alt="Toyota Yaris 2010 en Automarca Kia" /></a>
+		            </div>
+	            </div>
+                
+                <div class="box_detail_3">
+                    <div class="thumb_tiny"><a href="#http://midealervirtual.com/vpics/med/med_c6v2316t111006175246.JPG" title="Toyota Yaris 2010 en Automarca Kia"><img src="http://midealervirtual.com/vpics/tiny/tiny_c6v2316t111006175246.JPG" alt="Toyota Yaris 2010 en Automarca Kia" title="Toyota Yaris 2010 en Automarca Kia" /></a></div>
+                    <div class="thumb_tiny"><a href="#http://midealervirtual.com/vpics/med/med_c6v2316t111006175251.JPG" title="Toyota Yaris 2010 en Automarca Kia"><img src="http://midealervirtual.com/vpics/tiny/tiny_c6v2316t111006175251.JPG" alt="Toyota Yaris 2010 en Automarca Kia" title="Toyota Yaris 2010 en Automarca Kia" /></a></div>
+                    <div class="thumb_tiny"><a href="#http://midealervirtual.com/vpics/med/med_c6v2316t111006175257.JPG" title="Toyota Yaris 2010 en Automarca Kia"><img src="http://midealervirtual.com/vpics/tiny/tiny_c6v2316t111006175257.JPG" alt="Toyota Yaris 2010 en Automarca Kia" title="Toyota Yaris 2010 en Automarca Kia" /></a></div>
+                    <div class="thumb_tiny"><a href="#http://midealervirtual.com/vpics/med/med_c6v2316t111006175304.JPG" title="Toyota Yaris 2010 en Automarca Kia"><img src="http://midealervirtual.com/vpics/tiny/tiny_c6v2316t111006175304.JPG" alt="Toyota Yaris 2010 en Automarca Kia" title="Toyota Yaris 2010 en Automarca Kia" /></a></div>
+                    <div class="clear"></div>
+                </div>
+                
+                <div class="clear"></div>
+            
+            	<div class="box_detail_1">
+                    <ul>
+                        <li class="price"><strong>Precio:</strong> <span>$15,495.00</span></li>
+                        <li><strong>Tel&eacute;fono:</strong> <a>(888) 589-0636</a></li>
+                        <li><strong>A&ntilde;o:</strong> 2010</li>
+                        <li><strong>Color:</strong> Pacific Blue Metallic</li>
+                        <li><strong>Condici&oacute;n:</strong> Usado</li>
+                        <li><strong>Transmisi&oacute;n:</strong> Autom&aacute;tico</li>
+                        <li><strong>VIN:</strong> JTDBT9K37A1388240</li>
+                    </ul>
+                </div>
+            
+            </div>
+            <!-- end content -->
+		</div>
+        <!-- end main-->
+    </div>
+    <!-- end container -->
+</body>
+</html>
+
